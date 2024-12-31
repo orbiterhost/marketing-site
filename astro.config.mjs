d6ee5550786e1292bcbb7568e9734e9a7104b4a2
Steve <stevedsimkins@gmail.com>
2024-12-31 16:20:34 -0500

    feat: Added sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,16 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://orbiter.host",
-	output: "static",
-	build: {
-		assetsPrefix: "./",
-	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
-		mdx(),
-	],
+    site: "https://orbiter.host",
+    output: "static",
+    build: {
+        assetsPrefix: "./",
+    },
+    integrations: [tailwind({
+        applyBaseStyles: false,
+		}), react(), mdx(), sitemap()],
 });
