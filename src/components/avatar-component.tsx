1a09d04031f0c022d5ea3e82eef2b16cf68703a3
Steve <stevedsimkins@gmail.com>
2025-01-10 16:01:14 -0500

    feat: Added testimonials

--- src/components/avatar-component.tsx
+++ src/components/avatar-component.tsx
@@ -1,14 +1,20 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export const AvatarComponent = ({
-	src,
-	alt,
-	fallbackText,
-}: { src: string; alt: string; fallbackText: string }) => {
-	return (
-		<Avatar className="h-24 w-24">
-			<AvatarImage src={src} alt={alt} />
-			<AvatarFallback>{fallbackText}</AvatarFallback>
-		</Avatar>
-	);
+  src,
+  alt,
+  fallbackText,
+  className = "h-24 w-24"
+}: {
+  src: string;
+  alt: string;
+  fallbackText: string;
+  className?: string;
+}) => {
+  return (
+    <Avatar className={className}>
+      <AvatarImage src={src} alt={alt} />
+      <AvatarFallback>{fallbackText}</AvatarFallback>
+    </Avatar>
+  );
 };
